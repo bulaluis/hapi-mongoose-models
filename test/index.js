4906b1854b63fb014910f9e71be30ec3018a5dc9
Luis Miguel <bulaluis@gmail.com>
2015-10-13 18:51:04 -0500

    indent with spaces

--- test/index.js
+++ test/index.js
@@ -14,36 +14,36 @@ var server;
 
 lab.before(function (done) {
 
-	server = new Hapi.Server();
-	server.connection({ port: 3000 });
-	server.register({
-		register: Plugin,
-		options: {
-			pattern: '../models/**/*.js',
-			options: {
-				cwd: __dirname
-			}
-		}
-	}, function (err) {
+    server = new Hapi.Server();
+    server.connection({ port: 3000 });
+    server.register({
+        register: Plugin,
+        options: {
+            pattern: '../models/**/*.js',
+            options: {
+                cwd: __dirname
+            }
+        }
+    }, function (err) {
 
-		if (err) {
-			return done(err);
-		}
+        if (err) {
+            return done(err);
+        }
 
-		return done();
-	});
+        return done();
+    });
 });
 
 
 lab.experiment('Hapi-mongoose-models', function () {
 
-	lab.test('it returns models', function (done) {
+    lab.test('it returns models', function (done) {
 
-		Code.expect(server.plugins['hapi-mongoose-models']).to.be.an.object();
-		Code.expect(server.plugins['hapi-mongoose-models'].Normal).to.be.a.function();
-		Code.expect(server.plugins['hapi-mongoose-models'].Withfunction).to.be.a.function();
-		Code.expect(server.plugins['hapi-mongoose-models'].Nomodel).to.be.undefined();
+        Code.expect(server.plugins['hapi-mongoose-models']).to.be.an.object();
+        Code.expect(server.plugins['hapi-mongoose-models'].Normal).to.be.a.function();
+        Code.expect(server.plugins['hapi-mongoose-models'].Withfunction).to.be.a.function();
+        Code.expect(server.plugins['hapi-mongoose-models'].Nomodel).to.be.undefined();
 
-		return done();
-	});
+        return done();
+    });
 });
